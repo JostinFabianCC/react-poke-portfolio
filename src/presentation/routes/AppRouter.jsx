75087b2454c394c84dca0941c5565565c4e97925
JostinFabianCC <jostin20111@hotmail.com>
2025-04-21 19:20:45 +0200

    Add routes

--- src/presentation/routes/AppRouter.jsx
+++ src/presentation/routes/AppRouter.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
+import Pokedex from "../pages/Pokedex/Pokedex"; // ✅ Importación nueva
+
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="pokemon" element={<Pokedex />} /> {/* ✅ Nueva ruta */}
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
